166a2aeaf5ca8677fc8e076d7bf4ee4ff59a01a0
pedro-m-lima <pedromlima.pm@gmail.com>
2024-12-04 04:38:42 -0300

    Finalizacao aula refatoração
    Foi realizada a refatoração do coodigo onde o login passou a ser pre cadastro, e conforme as mudanças que aconteceram no sistema, foram realizados ajustes no código

--- web/cypress/e2e/login.cy.js
+++ web/cypress/e2e/login.cy.js
@@ -1,35 +1,31 @@
-describe('Login', () => {
+describe('Pré-Cadastro', () => {
 
   beforeEach(()=>{
     cy.visit('/')
   })
 
 
-  it('Realizar login com sucesso', () => {
+  it('Realizar pré-cadastro com sucesso', () => {
     
     //arrange
     const dadosLogin = {
       nameUser: 'Pedro Teste',
       emailUser: 'pedro@skills.com.br',
-      whatsUser: '14999999999'
     }
     
     //Act
-    cy.get('header nav div a[href="entrar"]')
+    cy.get('header nav div a[href="pre-cadastro"]')
       .click()   
 
     cy.get('form h2')
       .should('be.visible')
       .should('have.text', 'Seus dados')
-    cy.get('form input[placeholder="Nome"]')
+    cy.get('form input[name="nome"]')
       .type(dadosLogin.nameUser)
       .should('have.value', dadosLogin.nameUser)
-    cy.get('form input[placeholder="E-mail"]')
+    cy.get('form input[name="email"]')
       .type(dadosLogin.emailUser)
       .should('have.value', dadosLogin.emailUser)
-    cy.get('form input[placeholder="Whatsapp"]')
-      .type(dadosLogin.whatsUser)
-      .should('have.value', `(${dadosLogin.whatsUser.substring(0,2)}) ${dadosLogin.whatsUser.substring(2,7)}-${dadosLogin.whatsUser.substring(7,11)}`)
     cy.contains('form div button', 'Continuar')
       .click()
     cy.wait(100)
